42511d630fd0b7e2b65e0ddf8428bbbdd2062808
tuan <nguyenvietquoctuan@gmail.com>
2020-11-16 13:10:06 +0000

    v1

--- server.py
+++ server.py
@@ -19,34 +19,14 @@ debug = False
 app = Flask(__name__, static_url_path='')
 app.secret_key = b'\xd8t\xf3\x0b\x05\\\xc8\x80a\x8a\xe5\x16 \xd9\xf4d\x1dd\xa5\x9a\x82\xb6kh'
 
-connection = pymysql.connect(host='localhost',
-                             user='root',
-                             password='root',
-                             db='speaktests',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
 
-
-def reset_connection():
-    connection = pymysql.connect(host='localhost',
-                             user='root',
-                             password='root',
-                             db='speaktests',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
-
-def compute_duration(rows):
-    timestamps = list(rows["start_time"])
-    min_time, max_time = min(timestamps), max(timestamps)
-    duration = parser.parse(max_time) - parser.parse(min_time)
-    duration = duration.total_seconds()
-    
-    return pd.Series({
-        "start_time": min_time,
-        "end_time": min_time,
-        "log_count": len(rows),
-        "duration": duration
-    })
+def make_conn():
+    return pymysql.connect(host='localhost',
+                           user='root',
+                           password='root',
+                           db='speaktests',
+                           charset='utf8mb4',
+                           cursorclass=pymysql.cursors.DictCursor)
 
 
 @app.route('/')
@@ -55,68 +35,80 @@ def compute_duration(rows):
 def root():
     return app.send_static_file('index.html')
 
+
 @app.route('/log/', methods=['POST'])
 def log():
     data = request.get_json()
-    user_id, trial_id, passage_id, indx = data["user_id"], data["trial_id"], data["passage_id"], data["index"]
-    start_time = datetime.datetime.now().strftime(time_formt)
-    print(start_time)
+    user_id, session_id, paragraph_id, index = data["user_id"], data["session_id"], data["paragraph_id"], data["index"]
 
+    connection = make_conn()
     with connection.cursor() as cursor:
-        cursor.execute(f"INSERT INTO log (user_id, trial_id, passage_id, indx, start_time) VALUES ({user_id}, \"{trial_id}\", {passage_id}, {indx}, \"{start_time}\")")
-    connection.commit()
+        cursor.execute(f"INSERT INTO event (user_id, session_id, paragraph_id, word_index) VALUES ({user_id}, \"{session_id}\", {paragraph_id}, {index})")
+        connection.commit()
     return ""
 
-@app.route('/retrieve_history/', methods=['POST'])
-def retrieve_history():
-    reset_connection()
-    data = request.get_json()
-    user_id, passage_id = data["user_id"], data["passage_id"]
 
+@app.route('/create_user/<username>', methods=['POST'])
+def create_user(username):
+    user_id = -1
+
+    connection = make_conn()
     with connection.cursor() as cursor:
-        cursor.execute(f'SELECT * FROM passage WHERE id={passage_id}')
-        passage = cursor.fetchone()
-        length = len(passage["text"].split())
+        cursor.execute(f"INSERT INTO user (name) VALUES (\"{username}\")")
+        user_id = cursor.lastrowid
+        connection.commit()
+    return json.dumps({"user_id": user_id})
+
+
+@app.route('/retrieve_history/<user_id>/<paragraph_id>', methods=['POST'])
+def retrieve_history(user_id, paragraph_id):
+    sessions = []
+
+    connection = make_conn()
+    with connection.cursor() as cursor:
+        cursor.execute(f'SELECT * FROM paragraph WHERE id={paragraph_id}')
+        paragraph = cursor.fetchone()
+        length = len(paragraph["text"].split())
         
-        cursor.execute(f'SELECT trial_id, min(indx) as min_idx, max(indx) as max_idx, min(start_time) as start_time, max(start_time) as end_time FROM log WHERE user_id={user_id} AND passage_id={passage_id} GROUP BY trial_id;')
+        cursor.execute(f'SELECT session_id, min(word_index) as min_idx, max(word_index) as max_idx, min(start_time) as start_time, max(start_time) as end_time FROM event WHERE user_id={user_id} AND paragraph_id={paragraph_id} GROUP BY session_id;')
         result = cursor.fetchall()
         sessions = pd.DataFrame(result)
-        sessions = sessions[sessions.max_idx == length]
 
         if len(sessions) == 0:
             return "[]"
-
-        sessions["duration"] = sessions.apply(lambda row: (parser.parse(row.end_time) - 
-            parser.parse(row.start_time)).total_seconds(), axis=1)
+        sessions = sessions[sessions.max_idx == length]
+        if len(sessions) == 0:
+            return "[]"
+            
+        sessions["duration"] = sessions.apply(lambda row: (row.end_time - row.start_time).total_seconds(), axis=1)
         sessions = sessions.sort_values("start_time")
+        connection.commit()
+    return json.dumps(list(json.loads(sessions.transpose().to_json()).values()))
 
-        return json.dumps(list(json.loads(sessions.transpose().to_json()).values()))
 
-@app.route('/retrieve_passage/', methods=['POST'])
-def retrieve_passage():
-    data = request.get_json()
-    passage_id = data["id"]
+@app.route('/retrieve_paragraph/<id>', methods=['POST'])
+def retrieve_paragraph(id):
+    text = ""
 
+    connection = make_conn()
     with connection.cursor() as cursor:
-        cursor.execute(f"SELECT * FROM passage WHERE id={passage_id};")
-        result = cursor.fetchone()
+        cursor.execute(f"SELECT * FROM paragraph WHERE id={id};")
+        text = cursor.fetchone()["text"]
+        connection.commit()
+    return json.dumps({"paragraph": text})
 
-        return json.dumps({"passage": result["text"]})
 
-@app.route('/transcribe/', methods=['POST'])
-def transcribe():
-    data = request.get_json()
-    word = data["word"]
-    
+@app.route('/transcribe/<word>', methods=['POST'])
+def transcribe(word):
     response = requests.post(
         "https://www.phonetizer.com/phonetizer/default/call/jsonrpc?nocache=1605357656222",
         data = '{"service":"", "method":"transcribe", "id":5, "params":["' + word + '", "British", false]}'
     )
 
-    soup = BeautifulSoup(json.loads(response.content)["result"])
+    soup = BeautifulSoup(json.loads(response.content)["result"], features="html.parser")
     ipa = soup.get_text().strip().split("\n")[1]
-
     return json.dumps({"ipa": ipa})
+
 
 if __name__ == "__main__":
     #app.run(host="0.0.0.0", debug=True)
